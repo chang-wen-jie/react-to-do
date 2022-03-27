3a7160d03972bbe22c0536106edfabfe62047dbb
wen1jie <71407651+wen1jie@users.noreply.github.com>
2022-03-27 20:52:53 +0200

    update

--- src/components/todoForm/TodoForm.jsx
+++ src/components/todoForm/TodoForm.jsx
@@ -1,36 +1,72 @@
 import React from "react";
 import "./TodoForm.css";
 import IconButton from "@material-ui/core/IconButton";
-import AddBoxIcon from '@mui/icons-material/AddBox';
+import CreateIcon from '@mui/icons-material/Create';
+import DialogTitle from '@mui/material/DialogTitle';
+import Dialog from '@mui/material/Dialog';
 
-const styles = {
-  largeIcon: {
-    width: 600,
-    height: 600,
-  },
-
+const iconStyle = {
+  fontSize: 60,
+  color: "#964B00",
 };
 
-const TodoForm = ({ addTodo }) => {
+function SimpleDialog(props) {
+  const { onClose, open } = props;
+
   const [value, setValue] = React.useState("");
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!value) return;
-    addTodo(value);
+  const handleClose = () => {
+    onClose();
+  };
+
+  const handleSubmit = (value) => {
+    onClose(value);
     setValue("");
   };
 
+  const preventDefault = e => {
+    e.preventDefault();
+  };
+
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        className="input"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-      />
-      <IconButton iconStyle={styles.largeIcon} iconSize="400px"><AddBoxIcon /></IconButton>
-    </form>
+    <Dialog onClose={handleClose} open={open}>
+      <DialogTitle>Nieuwe taak</DialogTitle>
+      <form onSubmit={(e) => {handleSubmit(value); preventDefault(e)}}>
+        <input
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </form>
+    </Dialog>
+  );
+}
+
+const TodoForm = ({ addTodo }) => {
+  const value = React.useState("");
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (value) => {
+    setOpen(false);
+    if (value.trim() === "") return;
+    addTodo(value);
+  };
+
+  return (
+    <div style={{textAlign: "center", fontSize: 60, color: "#964B00"}}>
+        <IconButton onClick={handleClickOpen}>
+          <CreateIcon style={iconStyle} />
+        </IconButton>
+        <SimpleDialog
+          open={open}
+          onClose={handleClose}
+          value={value}
+        />
+    </div>
   );
 };
 
